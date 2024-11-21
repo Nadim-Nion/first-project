1f6dc40c44b5fdbd6f8d2a4367c2bebccb40675a
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-11-21 13:29:02 +0600

    student feature adde

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // const express = require('express')
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 // const port = 3000
 
@@ -8,10 +9,11 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
 app.get('/', (req: Request, res: Response) => {
-  // res.send('Hello World!')
-  const a = 10;
-  res.send(a);
+  res.send('Hello World!');
 });
 
 export default app;
