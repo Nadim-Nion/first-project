b62faf4ce8bfd62b1804fad490b872ecc6ac23ee
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-04-28 16:19:59 +0600

    handle department validation while creating and updating document

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -18,6 +18,32 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   },
 );
 
+academicDepartmentSchema.pre('save', async function (next) {
+  const isAcademicDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isAcademicDepartmentExist) {
+    throw new Error('Academic Department already exists!');
+  }
+
+  next();
+});
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  console.log(query);
+
+  const isAcademicDepartmentExist = await AcademicDepartment.findOne(query);
+  console.log(isAcademicDepartmentExist);
+
+  if (!isAcademicDepartmentExist) {
+    throw new Error('Academic Department does not exist!');
+  };
+
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
--- src/app/modules/academicDepartment/academicDepartment.service.ts
+++ src/app/modules/academicDepartment/academicDepartment.service.ts
@@ -2,16 +2,6 @@ import { TAcademicDepartment } from './academicDepartment.interface';
 import { AcademicDepartment } from './academicDepartment.model';
 
 const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
-  const isAcademicDepartmentExist = await AcademicDepartment.findOne({
-    name: payload.name,
-  });
-
-  if (isAcademicDepartmentExist) {
-    throw new Error('Academic Department already exists!');
-  }
-
-  /* I have watched the video of module (13-7) till 4:33 mins */
-
   const result = await AcademicDepartment.create(payload);
   return result;
 };
