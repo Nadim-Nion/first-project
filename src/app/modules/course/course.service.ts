d24e3345e41e0149f742f2b17897b57ecad18e4e
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-05-14 23:18:34 +0600

    dynamically add multiple pre-requisite courses

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -34,6 +34,7 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
   const { preRequisiteCourses, ...courseRemainingData } = payload;
 
   // Basic course info update
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
   const updatedBasicCourseInfo = await Course.findByIdAndUpdate(
     id,
     courseRemainingData,
@@ -48,18 +49,19 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
   // Check if there is or are pre-requisite courses to update
   if (preRequisiteCourses && preRequisiteCourses.length > 0) {
     // Filter out deleted pre-requisite courses
-    const filteredPreRequisiteCourses = preRequisiteCourses
+    const deletePreRequisites = preRequisiteCourses
       .filter((el) => el.course && el.isDeleted)
       .map((el) => el.course);
 
     // console.log('filteredPreRequisiteCourses:', filteredPreRequisiteCourses);
 
+    // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
     const deletedPreRequisiteCourses = await Course.findByIdAndUpdate(
       id,
       {
         $pull: {
           preRequisiteCourses: {
-            course: { $in: filteredPreRequisiteCourses },
+            course: { $in: deletePreRequisites },
           },
         },
       },
@@ -68,9 +70,29 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
         runValidators: true,
       },
     );
+
+    // Filter out pre-requisite courses that are not deleted
+    const newPreRequisites = preRequisiteCourses?.filter(
+      (el) => el.course && !el.isDeleted,
+    );
+
+    // console.log('newPreRequisites:', newPreRequisites);
+
+    // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+    const newPreRequisiteCourses = await Course.findByIdAndUpdate(id, {
+      $addToSet: {
+        preRequisiteCourses: {
+          $each: newPreRequisites,
+        },
+      },
+    });
   }
 
-  return updatedBasicCourseInfo;
+  const result = await Course.findById(id).populate(
+    'preRequisiteCourses.course',
+  );
+
+  return result;
 };
 
 const deleteCourseFromDB = async (id: string) => {
