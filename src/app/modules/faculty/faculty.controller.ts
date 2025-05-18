58bfdad9f7d62bf3b800386aeb9dfdafac4b4e8f
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-05-18 22:08:46 +0600

    retrive all faculties using service, controller and router file

---
+++ src/app/modules/faculty/faculty.controller.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from 'express';
+import catchAsync from '../../utils/catchAsync';
+import { FacultyService } from './faculty.service';
+import sendResponse from '../../utils/sendResponse';
+
+const getAllFaculties: RequestHandler = catchAsync(async (req, res) => {
+  const result = await FacultyService.getAllFacultiesFromDB();
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: 'All faculties retrieved successfully',
+    data: result,
+  });
+});
+
+export const FacultyController = {
+  getAllFaculties,
+};
