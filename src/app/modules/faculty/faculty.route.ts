033d7037296ae49814ae64fe900eeaac2f6c1f65
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-05-19 12:33:14 +0600

    add validateRequest for patch method

--- src/app/modules/faculty/faculty.route.ts
+++ src/app/modules/faculty/faculty.route.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import { FacultyController } from './faculty.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { FacultyValidation } from './faculty.validation';
 
 const router = express.Router();
 
 router.get('/', FacultyController.getAllFaculties);
 router.get('/:id', FacultyController.getSingleFaculty);
-router.patch('/:id', FacultyController.updateFaculty);
+router.patch(
+  '/:id',
+  validateRequest(FacultyValidation.updateFacultyValidationSchema),
+  FacultyController.updateFaculty,
+);
 
 export const FacultyRoutes = router;
--- src/app/modules/faculty/faculty.validation.ts
+++ src/app/modules/faculty/faculty.validation.ts
@@ -34,7 +34,9 @@ const createFacultyValidationSchema = z.object({
       email: z.string().email('Email is not valid'),
       contactNo: z.string(),
       emergencyContactNo: z.string(),
-      bloodGroup: z.enum([...BloodGroup] as [string, ...string[]], {message: '{VALUE} is not a valid blood group'}),
+      bloodGroup: z.enum([...BloodGroup] as [string, ...string[]], {
+        message: '{VALUE} is not a valid blood group',
+      }),
       presentAddress: z.string(),
       permanentAddress: z.string(),
       academicDepartment: z.string(),
@@ -43,6 +45,59 @@ const createFacultyValidationSchema = z.object({
   }),
 });
 
+const updateUserNameValidationSchema = z.object({
+  firstName: z
+    .string()
+    .min(1, 'First Name is required')
+    .max(20, 'First Name cannot exceed 20 characters')
+    .refine(
+      (value) =>
+        value.charAt(0).toUpperCase() + value.slice(1).toLowerCase() === value,
+      { message: 'First Name must be in capitalize format' },
+    )
+    .optional(),
+  middleName: z.string().optional(),
+  lastName: z
+    .string()
+    .min(1, 'Last Name is required')
+    .max(20, 'Last Name cannot exceed 20 characters')
+    .refine((value) => /^[a-zA-Z]+$/.test(value), {
+      message: 'Last Name is not valid',
+    })
+    .optional(),
+});
+
+const updateFacultyValidationSchema = z.object({
+  body: z.object({
+    password: z.string().min(1, 'Password is required').max(20).optional(),
+    faculty: z
+      .object({
+        designation: z.string().optional(),
+        name: updateUserNameValidationSchema.optional(),
+        gender: z
+          .enum([...Gender] as [string, ...string[]], {
+            message: '{VALUE} is not a valid gender',
+          })
+          .optional(),
+        dateOfBirth: z.string().optional(),
+        email: z.string().email('Email is not valid').optional(),
+        contactNo: z.string().optional(),
+        emergencyContactNo: z.string().optional(),
+        bloodGroup: z
+          .enum([...BloodGroup] as [string, ...string[]], {
+            message: '{VALUE} is not a valid blood group',
+          })
+          .optional(),
+        presentAddress: z.string().optional(),
+        permanentAddress: z.string().optional(),
+        academicDepartment: z.string().optional(),
+        profileImg: z.string().optional(),
+      })
+      .optional(),
+  }),
+});
+
 export const FacultyValidation = {
   createFacultyValidationSchema,
+  updateFacultyValidationSchema,
 };
