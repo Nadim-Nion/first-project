9766acf6f95747510f52bdb707cc869a41651e60
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-06-10 16:48:28 +0600

    verify token

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,15 +2,31 @@ import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
 import httpStatus from 'http-status';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    // Check whether the token is sent from the client
     const token = req.headers.authorization;
 
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
     }
-    next();
+
+    // Check whether the token is valid or not
+    jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+      function (err, decoded) {
+        // Check whether the token is mismatched or invalid
+        if (err) {
+          throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
+        }
+        req.user = decoded as JwtPayload;
+        next();
+      },
+    );
   });
 };
 
