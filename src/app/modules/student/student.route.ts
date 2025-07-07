d29ffd89eebd86fa78e3ad6dc9f4f6f6e9993013
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-07-07 10:30:25 +0600

    need a sperate /me route to get own data using own token without accesing other's data using my token

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -2,11 +2,17 @@ import express from 'express';
 import { StudentControllers } from './student.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { studentValidations } from './student.zod.validation';
+import auth from '../../middlewares/auth';
+import { UserRole } from '../user/user.constant';
 const router = express.Router();
 
 // will call controller function
 router.get('/', StudentControllers.getAllStudents);
-router.get('/:studentId', StudentControllers.getSingleStudent);
+router.get(
+  '/:studentId',
+  auth(UserRole.admin, UserRole.faculty),
+  StudentControllers.getSingleStudent,
+);
 router.patch(
   '/:studentId',
   validateRequest(studentValidations.updateStudentValidationSchema),
