c450402575b22f62a0b5b27368871b62186d6215
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-07-17 12:19:49 +0600

    create an student, login to get access token and validate the user using auth guard

--- src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -1,4 +1,15 @@
-const createEnrolledCourseIntoDB = async () => {};
+import { TEnrolledCourse } from './enrolledCourse.interface';
+
+const createEnrolledCourseIntoDB = async (
+  id: string,
+  payload: TEnrolledCourse,
+) => {
+  /*
+   1. Step-1: Check if the offeredCourse is exits or not
+   2. Step-2: Check if the student is already enrolled in the offeredCourse
+   3. Step-3: Create an enrolledCourse
+  */
+};
 
 export const EnrolledCourseServices = {
   createEnrolledCourseIntoDB,
