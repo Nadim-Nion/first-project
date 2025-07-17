0ee8a73df8ee093d49f6b0435f80005fcbfc3140
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-07-17 13:09:07 +0600

    enrolled student into the course

--- src/app/modules/enrolledCourse/enrolledCourse.service.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.service.ts
@@ -1,7 +1,12 @@
+import AppError from '../../errors/AppError';
+import { OfferedCourse } from '../offeredCourse/offeredCourse.model';
 import { TEnrolledCourse } from './enrolledCourse.interface';
+import httpStatus from 'http-status';
+import { EnrolledCourse } from './enrolledCourse.model';
+import { Student } from '../student/student.model';
 
 const createEnrolledCourseIntoDB = async (
-  id: string,
+  userId: string,
   payload: TEnrolledCourse,
 ) => {
   /*
@@ -9,6 +14,49 @@ const createEnrolledCourseIntoDB = async (
    2. Step-2: Check if the student is already enrolled in the offeredCourse
    3. Step-3: Create an enrolledCourse
   */
+
+  // Step-1
+  const { offeredCourse } = payload;
+  const isOfferedCourseExists = await OfferedCourse.findById(offeredCourse);
+  console.log('isOfferedCourseExists:', isOfferedCourseExists);
+
+  if (!isOfferedCourseExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Offered Course is not found');
+  }
+
+  // Check the current number of enrolled students
+  const enrolledCount = await EnrolledCourse.countDocuments({ offeredCourse });
+  console.log('Currently enrolled:', enrolledCount);
+  console.log('Max capacity:', isOfferedCourseExists.maxCapacity);
+
+  if (
+    isOfferedCourseExists.maxCapacity <= 0 &&
+    enrolledCount > isOfferedCourseExists.maxCapacity
+  ) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'Course enrollment capacity reached',
+    );
+  }
+
+  // Step-2
+  const isStudentExists = await Student.findOne({ id: userId });
+  console.log('isStudentExists:', isStudentExists);
+
+  if (!isStudentExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Student is not found');
+  }
+
+  const isStudentAlreadyEnrolled = await EnrolledCourse.findOne({
+    semesterRegistration: isOfferedCourseExists?.semesterRegistration,
+    offeredCourse,
+    student: isStudentExists._id,
+  });
+  console.log('isStudentAlreadyEnrolled:', isStudentAlreadyEnrolled);
+
+  if (isStudentAlreadyEnrolled) {
+    throw new AppError(httpStatus.CONFLICT, 'Student is already existed');
+  }
 };
 
 export const EnrolledCourseServices = {
