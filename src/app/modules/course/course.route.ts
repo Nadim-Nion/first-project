9942140ba4ab35b0f1b81be4cbd57be555a0ee6e
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-08-06 13:10:26 +0600

    create course using ph university syllabus

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -9,38 +9,48 @@ const router = express.Router();
 
 router.post(
   '/create-course',
-  auth(UserRole.admin),
+  auth(UserRole.superAdmin, UserRole.admin),
   validateRequest(CourseValidations.createCourseValidationSchema),
   CourseControllers.createCourse,
 );
 
-router.get('/', CourseControllers.getAllCourses);
+router.get(
+  '/',
+  auth(UserRole.superAdmin, UserRole.admin, UserRole.faculty, UserRole.student),
+  CourseControllers.getAllCourses,
+);
 
 router.get(
   '/:id',
-  auth(UserRole.admin, UserRole.faculty, UserRole.student),
+  auth(UserRole.superAdmin, UserRole.admin, UserRole.faculty, UserRole.student),
   CourseControllers.getSingleCourse,
 );
 
 router.patch(
   '/:id',
-  auth(UserRole.admin),
+  auth(UserRole.superAdmin, UserRole.admin),
   validateRequest(CourseValidations.updateCourseValidationSchema),
   CourseControllers.updateCourse,
 );
 
+router.delete(
+  '/:id',
+  auth(UserRole.superAdmin, UserRole.admin, UserRole.faculty, UserRole.student),
+  CourseControllers.deleteCourse,
+);
+
 router.put(
   '/:courseId/assign-faculties',
+  auth(UserRole.superAdmin, UserRole.admin),
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
   CourseControllers.assignFacultiesWithCourse,
 );
 
 router.delete(
   '/:courseId/remove-faculties',
+  auth(UserRole.superAdmin, UserRole.admin),
   validateRequest(CourseValidations.facultiesWithCourseValidationSchema),
   CourseControllers.removeFacultiesWithCourse,
 );
-
-router.delete('/:id', auth(UserRole.admin), CourseControllers.deleteCourse);
 
 export const CourseRoutes = router;
